480b463d84735c9b584a61d7da0bf5af2a3e0cc5
Vishad Mehta <144763669+mehta-vishad@users.noreply.github.com>
2024-06-16 17:13:46 -0400

    Connected backend - Digital ocean droplet

--- src/Chatbot.tsx
+++ src/Chatbot.tsx
@@ -45,7 +45,7 @@ const Chatbot: React.FC = () => {
 
     const conversationHistory = updatedMessages.map((message) => message.text);
 
-    const response = await fetch('http://127.0.0.1:8000/chat', {
+    const response = await fetch('http://165.227.105.139:8000/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ question: input, visitor_name: visitorName, visitor_company: visitorCompany, conversation_history: conversationHistory }),
