a3d00d4050d8a43056540d4582cb2f5b1c590169
Vishad Mehta <144763669+mehta-vishad@users.noreply.github.com>
2024-06-18 12:55:00 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Loader from './Loader';
 import Hero from './Hero';
 import Carousel from './Carousel';
 import Projects from './Projects';
@@ -12,6 +13,7 @@ import './Timeline.css';
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); // Start with loading set to true
   const [projects, setProjects] = useState([]);
 
   const toggleMode = () => {
@@ -21,30 +23,42 @@ const App: React.FC = () => {
   useEffect(() => {
     fetch('./projects.json')
       .then(response => response.json())
-      .then(data => setProjects(data))
-      .catch(error => console.error('Error loading project data:', error));
+      .then(data => {
+        setProjects(data);
+        setIsLoading(false); // Set loading to false once data is fetched
+      })
+      .catch(error => {
+        console.error('Error loading project data:', error);
+        setIsLoading(false); // Set loading to false even if there's an error
+      });
   }, []);
 
   return (
     <div className={darkMode ? 'dark-mode' : 'light-mode'}>
-      <Header darkMode={darkMode} toggleMode={toggleMode} />
-      <Hero darkMode={darkMode} />
-      <Carousel />
-      <section id="AMA">
-        <Chatbot />
-      </section>
-      <section id="aboutme">
-        <Timeline darkMode={darkMode} />
-      </section>
-      <section id="projects">
-        <Projects projects={projects} darkMode={darkMode} />
-      </section>
-      <section id="resume">
-        <Resume />
-      </section>
-      <section id="contact">
-        <Footer darkMode={darkMode} />
-      </section>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header darkMode={darkMode} toggleMode={toggleMode} />
+          <Hero darkMode={darkMode} />
+          <Carousel />
+          <section id="AMA">
+            <Chatbot />
+          </section>
+          <section id="aboutme">
+            <Timeline darkMode={darkMode} />
+          </section>
+          <section id="projects">
+            <Projects projects={projects} darkMode={darkMode} />
+          </section>
+          <section id="resume">
+            <Resume />
+          </section>
+          <section id="contact">
+            <Footer darkMode={darkMode} />
+          </section>
+        </>
+      )}
     </div>
   );
 };
